ac1db373cce755006f381f71ca6991fc30077b1d
rafaelmdalmoro <rafamascmoro@hotmail.com>
2020-08-09 20:27:17 -0300

    Update Sonar.ino

--- Brino/Sonar/Sonar.ino
+++ Brino/Sonar/Sonar.ino
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////
-//            ------ AutoCore Robótica -------         
-//               Projeto: Radar com HC-SR04            
+//            ------ AutoCore RobÃ³tica -------         
+//               Projeto: Sonar com HC-SR04            
 //               Autor do codigo em Arduino: Danilo Nogueira                
 //               Data: 20/08/2018                      
 //			  ------ Brino - Robotica Educacional ------
@@ -14,7 +14,7 @@
   const int  trig_Pino = 10;
   const int  echo_Pino = 11;
 
-  // Vamos criar duas variáveis para calcular a distancia do objeto
+  // Vamos criar duas variÃ¡veis para calcular a distancia do objeto
   long tempo;
   int  distancia;
 
@@ -23,42 +23,42 @@
 
 void setup(){
   
-  // Vamos começar definindo o trigger como saída
-  // e o Echo como entrada, além de iniciar a serial e o servo
+  // Vamos comeÃ§ar definindo o trigger como saÃ­da
+  // e o Echo como entrada, alÃ©m de iniciar a serial e o servo
   pinMode(trig_Pino, OUTPUT);
   pinMode(echo_Pino, INPUT);
 
   Serial.begin(9600);
-  Radar_Servo.attach(12); // Aqui eu defino em qual pino o servo está conectado
+  Radar_Servo.attach(12); // Aqui eu defino em qual pino o servo estÃ¡ conectado
 }
 
 void loop(){
   
-  // Aqui estamos fazendo um para para o servo girar de 15° até 165°
+  // Aqui estamos fazendo um para para o servo girar de 15Â° atÃ© 165Â°
     for(int  i = 0; i <= 180; i++){
     Radar_Servo.write(i);
     delay(30);
     
     // Nessa parte, enquanto o servo gira, o sensor verifica
     // Se tem algum objeto e qual a distancia
-    // Chamando a função 'CalculandoDistancia()'
+    // Chamando a funÃ§Ã£o 'CalculandoDistancia()'
     distancia = calculandoDistancia();
    
-    Serial.print(i);        // Mostra na serial qual o angulo de giro o servo está naquele momento
-    Serial.print(",");      // Aqui vamos separar o angulo da distancia calculada com uma vírgula
-    Serial.print(distancia); // Mostra a distancia que o sensor está calculando
+    Serial.print(i);        // Mostra na serial qual o angulo de giro o servo estÃ¡ naquele momento
+    Serial.print(",");      // Aqui vamos separar o angulo da distancia calculada com uma vÃ­rgula
+    Serial.print(distancia); // Mostra a distancia que o sensor estÃ¡ calculando
     Serial.print(".");      // Coloca um '.' depois da distancia for indexar no processing a distancia
     }
 
-    // Os mesmos passos que fizemos para ele ir de 15° --> 165°
-    // Vamos utilizar para ele voltar de 165° --> 15°
+    // Os mesmos passos que fizemos para ele ir de 15Â° --> 165Â°
+    // Vamos utilizar para ele voltar de 165Â° --> 15Â°
     for(int  i = 180; i > 0 ; i--){
     Radar_Servo.write(i);
     delay(30);
     
     // Nessa parte, enquanto o servo gira, o sensor verifica
     // Se tem algum objeto e qual a distancia
-    // Chamando a função 'CalculandoDistancia()'
+    // Chamando a funÃ§Ã£o 'CalculandoDistancia()'
     distancia = calculandoDistancia();
     
     Serial.print(i);
@@ -68,21 +68,21 @@ void loop(){
   }
 }
 
-    // Aqui vamos criar a função que utilizamos ali em cima
+    // Aqui vamos criar a funÃ§Ã£o que utilizamos ali em cima
     // Para poder calcular a distancia sabe?
     int  calculandoDistancia(){
  
     digitalWrite(trig_Pino, LOW);  // Aqui vou desligar o trigger for poder realizar os pulsos
     delayMicroseconds(2);
 
-    // Aqui é onde um ligo o trigger por 10 ms
-    // Que é o tempo para calcular a distancia
+    // Aqui Ã© onde um ligo o trigger por 10 ms
+    // Que Ã© o tempo para calcular a distancia
     digitalWrite(trig_Pino, HIGH);
     delayMicroseconds(10);
     digitalWrite(trig_Pino, LOW);
     
     tempo = pulseIn(echo_Pino, HIGH);  // Aqui setamos o Echo for capitar o pulso ultrassonico && voltar o tempo que ele demorou
     
-    distancia = tempo*0.034/2;  // Por fim, esta é a fórmula que utilizamos for converter o tempo na distancia do objeto até o sensor
+    distancia = tempo*0.034/2;  // Por fim, esta Ã© a fÃ³rmula que utilizamos for converter o tempo na distancia do objeto atÃ© o sensor
     return distancia;
 }
